bf47d137c81c859a90b7213721a4580e3c692306
Jack Gimre <jackgimre@gmail.com>
2026-01-11 17:30:03 -0500

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,21 @@
 import React, { useEffect, useState } from 'react';
-import { BrowserRouter as Router, Routes, Route, Link, useParams } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link, useParams, useNavigate } from "react-router-dom";
 import Home from './Home';
 import Calendar from './Calendar';
 import { fetchAllCalendarIds } from './utils/calendar';
 
 const CalendarWrapper = async () => {
+  const navigate = useNavigate();
   let { id } = useParams(); // id will be undefined if /calendar
-  if (id == undefined || id == null) {
+  if (id == undefined) {
     try {
       const ids = await fetchAllCalendarIds();
-      if (ids.length == 0) {
-        id = null;
-      } else {
-        id = ids[0];
+      if (ids.length !== 0) {
+        navigate(`/calendar/${ids[0]}`);
       }
     } catch (err) {
-      id = null;
+      id = undefined;
     }
-
-
   }
   return <Calendar calendarId={id} />;
 };
