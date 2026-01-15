1ef2576884878f4adc3cf876ea923f250b63f2d0
Jack Gimre <jackgimre@gmail.com>
2026-01-15 15:48:51 -0500

    Update proxy.js

--- client/src/utils/proxy.js
+++ client/src/utils/proxy.js
@@ -1,8 +1,5 @@
 const returnURL = () => {
-    const URL = process.env.NODE_ENV === "production"
-    ? 'https://todo-calendar-l2tb.onrender.com'
-    : 'http://localhost:4000';
-    return URL;
-}
+  return process.env.REACT_APP_API_URL || "http://localhost:4000";
+};
 
-export {returnURL};
+export { returnURL };
