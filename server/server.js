d8d1edb983fbe4de5545bd7163e83aa63eecad07
Jack Gimre <jackgimre@gmail.com>
2026-01-15 16:59:46 -0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -14,6 +14,22 @@ const app = express();
 
 const allowedOrigins = [process.env.CLIENT_URL, 'http://localhost:3000'];
 
+app.options(
+	/.*/,
+	cors({
+		origin: function (origin, callback) {
+			if (!origin) return callback(null, true);
+			if (allowedOrigins.includes(origin)) {
+				return callback(null, true);
+			}
+			callback(new Error('Not allowed by CORS'));
+		},
+		credentials: true,
+		methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+		allowedHeaders: ['Content-Type', 'Authorization']
+	})
+);
+
 app.use(
 	cors({
 		origin: function (origin, callback) {
@@ -31,9 +47,7 @@ app.use(
 
 app.use(cookieParser());
 app.use(express.json());
-
 app.use(morgan('dev'));
-app.use(express.json());
 
 try {
 	await mongoose.connect(process.env.MONGO_URI);
