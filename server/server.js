603ecd1c7a90d0f8a128dfc7569162475595779a
Jack Gimre <jackgimre@gmail.com>
2026-01-15 16:07:53 -0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -16,16 +16,17 @@
     "https://todo-calendar-plum-kappa.vercel.app/"
   ];
 
-  app.use(cors({
-    origin: (origin, callback) => {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error(`Origin ${origin} not allowed by CORS`));
-      }
-    },
-    credentials: true
-  }));
+app.use(cors({
+  origin: function(origin, callback) {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = `The CORS policy for this site does not allow access from the specified Origin.`;
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
+  credentials: true
+}));
 
   app.use(cookieParser());
   app.use(express.json());
